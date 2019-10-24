097f096d7070a2acbae52797b0b8a2013b9a55b5
Raul Gallo Dagir <rauldagir@hotmail.com>
2019-10-23 22:57:34 -0700

    Adding stuff to scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,12 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.alert import Alert
+import requests
 
 ALERT_CLASS_NAME="mt3GC" ## TODO - config file to store all these constants
-
+POST_OBJECT_CLASS_NAME="_8Rm4L M9sTE  L_LMM SgTZ1   ePUX4"
 class Scraper:
     def __init__():
         self.driver = webdriver.Chrome()
+        self.supportedImgFileTypes = ["image/jpeg"]
         return
 
     def connectToWebsite(self, url, cookies):
@@ -29,5 +31,23 @@ class Scraper:
             print("An exception was thrown while dismissing alert")
             return False
 
-    
+    def getAllPostsInScreen(self):
+        try:
+            posts = driver.find_elements_by_class_name(POST_OBJECT_CLASS_NAME)
+            return posts
+        except:
+            print("Could not fetch posts")
+            return None
 
+    def getImagesSaveLocally(self): 
+        try:
+            imgs = driver.find_elements_by_class_name("FFVAD")
+            for img in imgs:
+                img.get_attribute("srcset")
+                matched = re.search(r'https[^\s]*',toparse)
+                linkToImg = matched.group()
+                r = requests.get(linkToImg)
+                if (r.headers["Content-Type"] not in self.supportedImgFileTypes): return
+                open("testimg.png", "wb").write(r.content)
+                ## continue on and get further data on the post
+                # username, date, num of likes, num of comments, etc...
