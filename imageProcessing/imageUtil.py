5f8a8343fe9dc000eafaf64e9ccf86a8a8fd7eb0
PhilipPfeffer <philipmateopfeffer@Philips-MacBook-Pro.local>
2019-11-10 19:38:15 -0800

    Sector colour difference extractor

--- imageProcessing/imageUtil.py
+++ imageProcessing/imageUtil.py
@@ -2,6 +2,7 @@ import skimage
 import os
 from skimage import io
 import numpy as np
+from collections import defaultdict 
 
 class Image:
     def __init__(self, path, name):
@@ -10,6 +11,7 @@ class Image:
         self.imageArray = []
         self.imageShape = (0,0,0)
         self.imageProcess()
+        self.distanceCache = defaultdict(int)
 
     def getImageData(self):
         return (self.imageArray, self.imageShape)
@@ -42,43 +44,48 @@ class Image:
                 bTotal += self.imageArray[x][y][2]
         return (rTotal/numPix, gTotal/numPix, bTotal/numPix)
 
-    def sectorColours(self, numSectorsX, numSectorsY):
-        """Returns an imageArray with each sector's colour averaged out."""
+    def sectorColours(self, numSectorsX, numSectorsY, verbose = 0):
+        """Returns an imageArray and compact array with each sector's colour averaged out."""
         sectorArray = np.zeros(self.imageShape, dtype = np.uint8)
-        
         sectorWidth = self.imageShape[0]//numSectorsX
         sectorHeight = self.imageShape[1]//numSectorsY
         numSectors = numSectorsX*numSectorsY
+        compactArray = np.zeros((numSectors, 3), dtype = np.uint8)
         for n in range(numSectors):
-            print(f"Sector {n+1}/{numSectors}")
+            if verbose != 0: print(f"Sector {n+1}/{numSectors}")
             sectorR = 0
             sectorG = 0
             sectorB = 0
-            for x in range((n%numSectorsY) * sectorWidth, (n%numSectorsY + 1) * sectorWidth):
-                for y in range((n//numSectorsX) * sectorHeight, (n//numSectorsX + 1) * sectorHeight):
+            for x in range((n%numSectorsX) * sectorWidth, (n%numSectorsX + 1) * sectorWidth):
+                for y in range((n//numSectorsY) * sectorHeight, (n//numSectorsY + 1) * sectorHeight):
                     sectorR += self.imageArray[x][y][0]
                     sectorG += self.imageArray[x][y][1]
                     sectorB += self.imageArray[x][y][2]
-            
             sectorR /= (sectorWidth*sectorHeight)
             sectorG /= (sectorWidth*sectorHeight)
             sectorB /= (sectorWidth*sectorHeight)
-            for x in range((n%numSectorsY) * sectorWidth, (n%numSectorsY + 1) * sectorWidth):
-                for y in range((n//numSectorsX) * sectorHeight, (n//numSectorsX + 1) * sectorHeight):
+            compactArray[n][0] = sectorR
+            compactArray[n][1] = sectorG
+            compactArray[n][2] = sectorB
+            for x in range((n%numSectorsX) * sectorWidth, (n%numSectorsX + 1) * sectorWidth):
+                for y in range((n//numSectorsY) * sectorHeight, (n//numSectorsY + 1) * sectorHeight):
                     sectorArray[x][y][0] = sectorR
                     sectorArray[x][y][1] = sectorG
                     sectorArray[x][y][2] = sectorB
         
-        return sectorArray
+        return (sectorArray, compactArray)
 
-
-        # newImageArray = np.zeros(self.imageShape, dtype = np.uint8)
-        # for x in range(self.imageShape[0]):
-        #     for y in range(self.imageShape[1]):
-        #         newImageArray[x,y,0] = int(avgPixs[0])
-        #         newImageArray[x,y,1] = int(avgPixs[1])
-        #         newImageArray[x,y,2] = int(avgPixs[2])
-
+    def calculateColourDistance(self, compactArr, numSectorsX, numSectorsY, sector1, sector2):
+        if self.distanceCache[(numSectorsX, numSectorsY, sector1, sector2)] != 0:
+            return self.distanceCache[(numSectorsX, numSectorsY, sector1, sector2)]
+        if self.distanceCache[(numSectorsX, numSectorsY, sector2, sector1)] != 0:
+            return self.distanceCache[(numSectorsX, numSectorsY, sector2, sector1)]
+        distance = 0
+        distance += abs(int(compactArr[sector1][0]) - compactArr[sector2][0])
+        distance += abs(int(compactArr[sector1][1]) - compactArr[sector2][1])
+        distance += abs(int(compactArr[sector1][2]) - compactArr[sector2][2])
+        self.distanceCache[(numSectorsX, numSectorsY, sector1, sector2)] = distance
+        return distance
 
 def saveNewAverageImage(name, avgPixs, shape):
     """Saves new image with all pixels avaraged, provided the average (r,g,b) pixel values"""
--- imageProcessing/imageprocess.py
+++ imageProcessing/imageprocess.py
@@ -1,24 +1,41 @@
 import imageUtil
 from imageUtil import Image
+import numpy as np
 
 imagesCreated = []
 
-# Average out all pixel colours
 def averageTest(image):
+    """Average out all pixel colours."""
     newImageName = 'averageImage.jpg'
     averagePixs = image.getAverageRGB()
     imageUtil.saveNewAverageImage(newImageName, averagePixs, image.imageShape)
     imagesCreated.append(newImageName)
 
-def colourful(path, name, numSectorsX=30, numSectorsY=30):
+def derezzed(image, numSectorsX, numSectorsY, verbose=0):
+    """Averages out pixel over numSectorsX*numSectorsY sectors."""
     newImageName = 'sectorCols.jpg'
-    sectorCols = image.sectorColours(numSectorsX, numSectorsY)
+    sectorCols, compactArr = image.sectorColours(numSectorsX, numSectorsY, verbose)
     imageUtil.saveNewImage(newImageName, sectorCols)
     imagesCreated.append(newImageName)
+    return compactArr
+
+def extractSectorsFeature(image, numSectorsX=30, numSectorsY=30):
+    """A list of the colour distance of each sector."""
+    compactArr = derezzed(image, numSectorsX, numSectorsY, 1)
+    numSectors = numSectorsX*numSectorsY
+    sectorColourDistance = np.zeros(numSectors, dtype = np.int64)
+    for n in range(numSectors):
+        colourDistance = 0
+        colourDistance += image.calculateColourDistance(compactArr, numSectorsX, numSectorsY, n, n-1) if n%numSectorsX != 0 else 0
+        colourDistance += image.calculateColourDistance(compactArr, numSectorsX, numSectorsY, n, n+1) if n%numSectorsX != numSectorsX-1 else 0
+        colourDistance += image.calculateColourDistance(compactArr, numSectorsX, numSectorsY, n, n-numSectorsX) if n//numSectorsY != 0 else 0
+        colourDistance += image.calculateColourDistance(compactArr, numSectorsX, numSectorsY, n, n+numSectorsX) if n//numSectorsY != numSectorsY-1 else 0
+        colourDistance /= 4
+        sectorColourDistance[n] = colourDistance
+    return sectorColourDistance
 
 if __name__ == "__main__":
     path = './'
     name = 'testimage.jpg'
     image = Image(path, name)
-    # averageTest(image)
-    colourful(path, name)
+    extractSectorsFeature(image, 8, 8)
