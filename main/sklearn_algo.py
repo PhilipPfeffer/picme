78b33ad278b5d23c4dc9f045fab91546f7d93086
Raul Gallo Dagir <rauldagir@hotmail.com>
2019-11-14 18:03:45 -0800

    Updating sklearn_algo to include runWithoutFeature, while also adding spreadsheet to create accuracy graphs

--- main/sklearn_algo.py
+++ main/sklearn_algo.py
@@ -13,17 +13,65 @@ class SKLearnPredictor:
         self.trainData, self.testData = extractFeaturesFromDataset('datasets/thegreatdataset.csv')
 
     def run(self):
+        # general for both test and train
         label_names = ["instagram worthy", "shit"]
-        labels = [ex[0] for ex in trainData]
-        feature_names = list(trainData[0][0].keys())
+        feature_names = list(self.trainData[0][0].keys())
+
+        # train
+        train_labels = [ex[1] for ex in self.trainData]
+        train = [list(ex[0].values()) for ex in self.trainData]
+
+        # test
+        test_labels =  [ex[1] for ex in self.testData]
+        test = [list(ex[0].values()) for ex in self.testData]
+
         # Initialize our classifier
         gnb = GaussianNB()
+
         # Train our classifier
-        model = gnb.fit(trainData, train_labels)
+        model = gnb.fit(train, train_labels)
 
         # Make predictions
-        preds = gnb.predict(testData)
+        preds = gnb.predict(test)
+        print(preds)
+        print(f"weights: {gnb.get_params()}")
+
+        # Evaluate accuracy
+        print("GaussianNB Prediction Accuracy: " + str(accuracy_score(test_labels, preds)))
+
+    def runWithoutFeature(self, delIndex):
+        # general for both test and train
+        label_names = ["instagram worthy", "shit"]
+        feature_names = list(self.trainData[0][0].keys())
+        print(f"deleting feature: {feature_names[delIndex]}")
+        del feature_names[delIndex]
+
+        # train
+        train_labels = [ex[1] for ex in self.trainData]
+        train = [list(ex[0].values()) for ex in self.trainData]
+        for t in train:
+            del t[delIndex]
+
+        # test
+        test_labels =  [ex[1] for ex in self.testData]
+        test = [list(ex[0].values()) for ex in self.testData]
+        for tst in test:
+            del tst[delIndex]
+
+        # Initialize our classifier
+        gnb = GaussianNB()
+
+        # Train our classifier
+        model = gnb.fit(train, train_labels)
+
+        # Make predictions
+        preds = gnb.predict(test)
         print(preds)
 
-# Evaluate accuracy
-print(accuracy_score(test_labels, preds))
+        # Evaluate accuracy
+        print("GaussianNB Prediction Accuracy: " + str(accuracy_score(test_labels, preds)))
+
+if __name__ == "__main__":
+    sk = SKLearnPredictor()
+    sk.loadDataset()
+    sk.run()
